3eefad808456f4e8c18987e34726f6a1c0c46cd6
Thibaud Lutellier <thibaud.lutellier@gmail.com>
2023-03-15 13:33:45 -0600

    Fix bugs in both file

--- simpleSubHacker.py
+++ simpleSubHacker.py
@@ -1,153 +1,147 @@
-''' Substitution cipher hacker that uses word patterns to decrypt. Must run 
-	makeWordPattersns.py first to create wordPatterns.py module. Additionally,
-	requires dictionary.txt'''
-
-#import wordPatterns.py
-# if ^^ gives ModuleNotFoundError, input the following
-#import sys
-#sys.path.append('Folder_where_wordPatterns_is)
-
-import os
-import re
-import copy
-import pyperclip
-import simpleSubCipher
-import wordPatterns
-import makeWordPatterns
+# Simple Substitution Cipher Hacker
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+import os, re, copy, wordPatterns, makeWordPatterns
+import simpleSubstitutionCipher as simpleSubCipher
 
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 nonLettersOrSpacePattern = re.compile('[^A-Z\s]')
 
+
 def main():
-	message = '''Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr
-        sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, lxo txmarr jia aqsoaxwa
-        sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac
-        ilxo, ia sr pyyacao rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx
-        lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia
-        rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh.
-        -Facjclxo Ctrramm'''
-
-    # Determine the possible valid ciphertext translations
-    print('Attempting to hack....')
+    message = "KJJY HLDP VOP UOLHVOLNQ GPPY PVIPOG, UVOILDWNVONQ GPPYG JE IOPPG GWDM VG JVF, APPDM, VGM, NLHP, MVKIMJOX, VXY GQDVHJOP. LE GPPYG VOP UNPXILEWN JX IMP BOJWXY, IMPQ DVOOQ IMPH AVDF IJ IMPLO XPGIG/AWOOJKG EJO GIJOVBP. IMPQ HVQ PVI GHVNN LXTPOIPAOVIPG GWDM VG GXVLNG VXY LXGPDIG, UVOILDWNVONQ LX NVIP GUOLXB VXY PVONQ GWHHPO KMPX GPPYG VOP NPVGI VTVLNVANP. NVIPO LX IMP GPVGJX IMPQ KLNN PVI APOOLPG, EOWLIG, EWXBL VXY OJJIG. LX KLXIPO, IMPQ HVQ UOPQ JX MLAPOXVILXB AVIG."   # Determine the possible valid ciphertext translations:
+    print('Hacking...')
     letterMapping = hackSimpleSub(message)
-
-    # Display to user
+    # Display the results to the user:
     print('Mapping:')
     print(letterMapping)
     print()
     print('Original ciphertext:')
     print(message)
     print()
-    print('Copying hacked message to clipboard:')
-    hackedMessage = decryptWithCipherLetterMapping(message, letterMapping)
-    pyperclip.copy(hackedMessage)
+    print('Print hacked message:')
+    hackedMessage = decryptWithCipherletterMapping(message, letterMapping)
     print(hackedMessage)
 
-def getBlankCipherLetterMapping():
-	return {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [],
-    'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [],
-    'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [],
-    'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []}
 
-''' Takes a dictionary value that stores a cipherletter mapping, 
-	which is copied by the function. The cipherword parameter is 
-	a string value of the ciphertext word. The candidate parameter
-	is a possible English word that the cipherword could decrypt
-	tp. This function adds the letters in the candidate as
-	potential decryption letters for the cipherletters in the
-	cipherletter mapping '''
+def getBlankCipherletterMapping():
+    # Returns a dictionary value that is a blank cipherletter mapping:
+    return {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [],
+            'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [],
+            'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [],
+            'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []}
+
+
 def addLettersToMapping(letterMapping, cipherword, candidate):
-	for i in range(len(cipherword)):
-		if candidate[i] not in letterMapping[cipherword[i]]:
-			letterMapping[cipherword[i]].append(candidate[i])
+    # The letterMapping parameter takes a dictionary value that
+    # stores a cipherletter mapping, which is copied by the function.
+    # The cipherword parameter is a string value of the ciphertext word.
+    # The candidate parameter is a possible English word that the
+    # cipherword could decrypt to.
 
-''' To intersect two maps, create a blank map and then add only the 
-	potential decryption letters if they exist in BOTH maps'''
+    # This function adds the letters in the candidate as potential
+    # decryption letters for the cipherletters in the cipherletter
+    # mapping.
+    for i in range(len(cipherword)):
+        if candidate[i] not in letterMapping[cipherword[i]]:
+            letterMapping[cipherword[i]].append(candidate[i])
+
+
 def intersectMappings(mapA, mapB):
-	intersectedMapping = getBlankCipherLetterMapping()
-	for letter in LETTERS:
-		# An empty list means any letter is possible. In this case
-		# just copy the other map entirely
-		if mapA[letter] == []:
-			intersectedMapping[letter] = copy.deepcopy(mapB[letter])
-		elif mapB[letter] == []:
-			intersectedMapping[letter] = copy.deepcopy(mapA[letter])
-		else:
-			# If a letter in mapA[letter] exists in mapB[letter], add
-			# that letter to intersectedMapping[letter]
-			for mappedLetter in mapA[letter]:
-				if mappedLetter in mapB[letter]:
-					intersectedMapping[letter].append(mappedLetter)
+    # To intersect two maps, create a blank map and then add only the
+    # potential decryption letters if they exist in BOTH maps:
+    intersectedMapping = getBlankCipherletterMapping()
+    for letter in LETTERS:
+        # An empty list means "any letter is possible". In this case just
+        # copy the other map entirely:
+        if mapA[letter] == []:
+            intersectedMapping[letter] = copy.deepcopy(mapB[letter])
+        elif mapB[letter] == []:
+            intersectedMapping[letter] = copy.deepcopy(mapA[letter])
+        else:
+            # If a letter in mapA[letter] exists in mapB[letter],
+            # add that letter to intersectedMapping[letter]:
+            for mappedLetter in mapA[letter]:
+                if mappedLetter in mapB[letter]:
+                    intersectedMapping[letter].append(mappedLetter)
+    return intersectedMapping
 
-	return intersectedMapping
 
-''' Cipherletters in the mapping that map to only one letter are 'solved'
-	and can be removed from the other letters. EX: if 'A' maps to potential
-	letters ['M', 'N'] and 'B' maps to ['N'], we know that 'B' must map to 'N'
-	so we can remove 'N' from the list of what 'A' could map to.'''
-def removedSolvedLettersFromMapping(letterMapping):
-	loopAgain = True
-	while loopAgain:
-		loopAgain = False
+def removeSolvedLettersFromMapping(letterMapping):
+    # Cipherletters in the mapping that map to only one letter are
+    # "solved" and can be removed from the other letters.
+    # For example, if 'A' maps to potential letters ['M', 'N'], and 'B'
+    # maps to ['N'], then we know that 'B' must map to 'N', so we can
+    # remove 'N' from the list of what 'A' could map to. So 'A' then maps
+    # to ['M']. Note that now that 'A' maps to only one letter, we can
+    # remove 'M' from the list of letters for every other letter.
+    # (This is why there is a loop that keeps reducing the map.)
 
-		# Solved letters will be a list of uppercase letters that have one
-		# and only one possible mapping in letterMapping
-		solvedLetters = []
-		for cipherletter in LETTERS:
-			if len(letterMapping[cipherletter]) == 1:
-				solvedLetters.append(letterMapping[cipherletter][0])
+    loopAgain = True
+    while loopAgain:
+        # First assume that we will not loop again:
+        loopAgain = False
 
-		# If a letter is solved, it can't be a match for oher ciphertext
-		# letters
-		for cipherletter in LETTERS:
-			for s in solvedLetters:
-				if len(letterMapping[cipherletter]) != 1 and s in letterMapping[cipherletter]:
-					letterMapping[cipherletter].remove(s)
-					if len(letterMapping[cipherletter]) == 1:
-						# A new letter is solved, so loop again
-						loopAgain = True
+        # solvedLetters will be a list of uppercase letters that have one
+        # and only one possible mapping in letterMapping:
+        solvedLetters = []
+        for cipherletter in LETTERS:
+            if len(letterMapping[cipherletter]) == 1:
+                solvedLetters.append(letterMapping[cipherletter][0])
 
-	return letterMapping
+        # If a letter is solved, then it cannot possibly be a potential
+        # decryption letter for a different ciphertext letter, so we
+        # should remove it from those other lists:
+        for cipherletter in LETTERS:
+            for s in solvedLetters:
+                if len(letterMapping[cipherletter]) != 1 and s in letterMapping[cipherletter]:
+                    letterMapping[cipherletter].remove(s)
+                    if len(letterMapping[cipherletter]) == 1:
+                        # A new letter is now solved, so loop again:
+                        loopAgain = True
+    return letterMapping
+
 
 def hackSimpleSub(message):
-	intersectedMap = getBlankCipherLetterMapping()
-	cipherwordList = nonLettersOrSpacePattern.sub('', message.upper()).split()
-	for cipherword in cipherwordList:
-		# Get a new cipherletter mapping for each ciphertext worfd
-		candidateMap = getBlankCipherLetterMapping()
+    intersectedMap = getBlankCipherletterMapping()
+    cipherwordList = nonLettersOrSpacePattern.sub('', message.upper()).split()
+    for cipherword in cipherwordList:
+        # Get a new cipherletter mapping for each ciphertext word:
+        candidateMap = getBlankCipherletterMapping()
 
-		wordPattern = makeWordPatterns.getWordPattern(cipherword)
-		if wordPattern not in wordPatterns.allPatterns:
-			continue
+        wordPattern = makeWordPatterns.getWordPattern(cipherword)
+        if wordPattern not in wordPatterns.allPatterns:
+            continue  # This word was not in our dictionary, so continue.
 
-		# Add the letters of each candidate to the mapping
-		for candidate in wordPatterns.allPatterns[wordPattern]:
-			addLettersToMapping(candidateMap, cipherword, candidate)
+        # Add the letters of each candidate to the mapping:
+        for candidate in wordPatterns.allPatterns[wordPattern]:
+            addLettersToMapping(candidateMap, cipherword, candidate)
 
-		# Intersect thenew mapping with the existing intersected mapping
-		intersectedMap = intersectMappings(intersectedMap, candidateMap)
+        # Intersect the new mapping with the existing intersected mapping:
+        intersectedMap = intersectMappings(intersectedMap, candidateMap)
 
-	# Remove any solved letters from the other lists
-	return removedSolvedLettersFromMapping(intersectedMap)
+    # Remove any solved letters from the other lists:
+    return removeSolvedLettersFromMapping(intersectedMap)
 
-def decryptWithCipherLetterMapping(ciphertext, letterMapping):
-	# Return a string of the ciphertext decrypted with the letter mapping, 
-	# with any ambiguous decrypted letters replaced with an underscore
 
-	# First create a simple sub key from the letterMapping mapping
-	key = ['x'] * len(LETTERS)
-	for cipherletter in LETTERS:
-		if len(letterMapping[cipherletter]) == 1:
-			# If there's only one letter, add it to the key:
-			keyIndex = LETTERS.find(letterMapping[cipherletter][0])
-			key[keyIndex = cipherletter]
-		else:
-			ciphertext = ciphertext.replace(cipherletter.lower(), '_')
-			ciphertext = ciphertext.replace(cipherletter.upper(), '_')
-	key = ''.join(key)
+def decryptWithCipherletterMapping(ciphertext, letterMapping):
+    # Return a string of the ciphertext decrypted with the letter mapping,
+    # with any ambiguous decrypted letters replaced with an underscore.
 
-	# Now, with the key, decrypt the ciphertext
-	return simpleSubCipher.decryptMessage(key, ciphertext)
+    # First create a simple sub key from the letterMapping mapping:
+    key = ['x'] * len(LETTERS)
+    for cipherletter in LETTERS:
+        if len(letterMapping[cipherletter]) == 1:
+            # If there's only one letter, add it to the key:
+            keyIndex = LETTERS.find(letterMapping[cipherletter][0])
+            key[keyIndex] = cipherletter
+        else:
+            ciphertext = ciphertext.replace(cipherletter.lower(), '_')
+            ciphertext = ciphertext.replace(cipherletter.upper(), '_')
+    key = ''.join(key)
+    print(key)
+    # With the key we've created, decrypt the ciphertext:
+    return simpleSubCipher.decryptMessage(key, ciphertext)
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- simpleSubstitutionCipher.py
+++ simpleSubstitutionCipher.py
@@ -1,46 +1,26 @@
-''' Simple substitution cipher that contains enough keys that it 
-	can thwart a bruteforce attack'''
-
-import sys
-import random
-import pyperclip
-
+import sys, random
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 def main():
-	myMessage = '''If a man is offered a fact which goes against his
-        instincts, he will scrutinize it closely, and unless the evidence
-        is overwhelming, he will refuse to believe it. If, on the other
-        hand, he is offered something which affords a reason for acting
-        in accordance to his instincts, he will accept it even on the
-        slightest evidence. The origin of myths is explained in this way.
-        -Bertrand Russell'''
-
-    myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
-    # Can be 'encrypt' or 'decrypt'
-    myMode = 'encrypt'
-
-    if not keyIsValid(myKey):
-      	sys.exit('There is an error in the key or symbol set')
-    if myMode == 'encrypt':
-        translated = encryptMessage(myKey, myMessage)
-    elif myMode == 'decrypt':
-    	translated = decryptMessage(myKey, myMessage) 
-    print('Using Key: %s' % (myKey))
+	myMessage = 'If'
+	myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
+	myMode = 'encrypt' # Set to 'encrypt' or 'decrypt'.
+	if keyIsValid(myKey):
+		sys.exit('There is an error in the key or symbol set.')
+	if myMode == 'encrypt':
+		translated = encryptMessage(myKey, myMessage)
+	elif myMode == 'decrypt':
+		translated = decryptMessage(myKey, myMessage)
+	print('Using key %s' % (myKey))
 	print('The %sed message is:' % (myMode))
 	print(translated)
-	pyperclip.copy(translated)
-	print('This message has been copied to clipboard.')
 
 def keyIsValid(key):
 	keyList = list(key)
 	lettersList = list(LETTERS)
 	keyList.sort()
 	lettersList.sort()
-
 	return keyList == lettersList
-
-def encryptMesage(key, message):
+def encryptMessage(key, message):
 	return translateMessage(key, message, 'encrypt')
 
 def decryptMessage(key, message):
@@ -50,23 +30,22 @@ def translateMessage(key, message, mode):
 	translated = ''
 	charsA = LETTERS
 	charsB = key
-
 	if mode == 'decrypt':
-		# If decrypting, only have to swap LETTERS with KEY
-		charA, charsB = charsB, charsA
-
-	# Loop over each symbol
-	for symbol in message:
-		if symbol.upper() in charsA:
-			# Encrypt or decrypt
-			symIndex = charsA.find(symbol.upper())
-			if symbol.isupper():
-				translated += charsB[symIndex].upper()
+		# For decrypting, we can use the same code as encrypting. We
+		# just need to swap where the key and LETTERS strings are used.
+		charsA, charsB = charsB, charsA
+		# Loop through each symbol in the message:
+		for symbol in message:
+			if symbol.upper() in charsA:
+				# Encrypt/decrypt the symbol:
+				symIndex = charsA.find(symbol.upper())
+				if symbol.isupper():
+					translated += charsB[symIndex].upper()
+				else:
+					translated += charsB[symIndex].lower()
 			else:
-				translated += charsB[symIndex].lower()
-		else:
-			# If symbol is not in letters, just append it
-			translated += symbol
+				# Symbol is not in LETTERS; just add it:
+				translated += symbol
 
 	return translated
 
@@ -75,6 +54,6 @@ def getRandomKey():
 	random.shuffle(key)
 	return ''.join(key)
 
-# If ran instead of imported
 if __name__ == '__main__':
 	main()
+
