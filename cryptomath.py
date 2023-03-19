307a4a9518c627fa5d7d0fcd031a6496a08d7443
Thibaud Lutellier <thibaud.lutellier@gmail.com>
2023-03-19 15:11:01 -0600

    update code for A5

--- cryptomath.py
+++ cryptomath.py
@@ -1,27 +1,24 @@
-''' Python module that includes functions for finding the greatest common 
-	divisor of two ints (Euclid's) and finding the modular inverse of two
-	ints.'''
+# Cryptomath Module
+# http://inventwithpython.com/hacking (BSD Licensed)
 
-# Greatest common divisor using Euclid's algorthm
 def gcd(a, b):
-	while a!= 0:
-		a, b = b % a, a
-	return b
+    # Return the GCD of a and b using Euclid's Algorithm
+    while a != 0:
+        a, b = b % a, a
+    return b
 
-# Return the modulus inverse of a % m, which is the number
-# x such that A * x % m = 1 (relative prime)
+
 def findModInverse(a, m):
+    # Returns the modular inverse of a % m, which is
+    # the number x such that a*x % m = 1
 
-	# If a and m are not relatively prime, no mod inverse
-	if gcd(a, m) != 1:
-		return None
+    if gcd(a, m) != 1:
+        return None # no mod inverse if a & m aren't relatively prime
 
-	# Calculate using the extended Euclidean algorithm
-	u1, u2, u3 = 1, 0, a
-	v1, v2, v3 = 0, 1, m
-	while v3 != 0:
-		# Integer division
-		q = u3 // v3
-		v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2),
-		(u3 - q * v3), v1, v2, v3
-	return u1 % m
+    # Calculate using the Extended Euclidean Algorithm:
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3 # // is the integer division operator
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
